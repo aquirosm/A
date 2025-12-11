9474f66cbe6fea62ffe0107ab93373b4c3dd8f00
Alejandro Quiros <alejandro_quiros@mckinsey.com>
2025-12-11 10:50:44 -0600

    frist commit

---
+++ test.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+
+variable_1 = "this is fine"
+variable_2 = 6/3
+
+import pyspark
+print(pyspark.__version__)
+
+
+continents = ["america", "europe", "asia", "africa", "oceania", "antartic"]
+print(continents)
+
+
+world = dict({'country':[
+    'italy',
+    'austria',
+    'hungary',
+    'kenya',
+    'brazil',
+    'india'],
+              'capital':[
+    'rome',
+    'viena',
+    'budapest',
+    'nairobi',
+    'brasilia',
+    'new delhi']})
+
---
+++ test2.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+
+def my_function(name, age):
+    return print("Hello, my name is " + name + " and my age is " + str(age))
+
+
+my_function(name="Alejandro", age=35)
+
